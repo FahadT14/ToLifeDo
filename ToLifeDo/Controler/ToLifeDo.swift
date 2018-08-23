b8931bd9eff4e9bb739d8fa79eb4e0587b891922
Fahad Tariq <official_fahadt@hotmail.com>
2018-08-24 00:06:02 +0500

    Item version of saving new To do list

--- ToLifeDo/Controler/ToLifeDo.swift
+++ ToLifeDo/Controler/ToLifeDo.swift
@@ -11,7 +11,7 @@ import UIKit
 class ToLifeDo: UITableViewController {
     
     
-    var taskArray = ["Any thing", "Hello Baby", "What's up"]
+    var taskArray = [TodoItem]()
     
     let defaults = UserDefaults.standard
     
@@ -19,9 +19,22 @@ class ToLifeDo: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let item = defaults.array(forKey: "TodoListArray") as? [String] {
+        let newItem = TodoItem()
+        newItem.item = "Fahad"
+        taskArray.append(newItem)
+        
+        let newItem1 = TodoItem()
+        newItem1.item = "Ali"
+        taskArray.append(newItem1)
+        
+        let newItem2 = TodoItem()
+        newItem2.item = "Mustafa"
+        taskArray.append(newItem2)
+        
+        
+        if let item = defaults.array(forKey: "TodoListArray") as? [TodoItem] {
             taskArray = item
-            
+
         }
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -33,7 +46,19 @@ class ToLifeDo: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "TaskToDo", for: indexPath)
         
-        cell.textLabel?.text = taskArray[indexPath.row]
+        let item = taskArray[indexPath.row]
+        
+        cell.textLabel?.text = item.item
+        
+        cell.accessoryType = item.done == true ? .checkmark : .none
+        
+//        if item.done == true {
+//            cell.accessoryType = .checkmark
+//        }else {
+//
+//            cell.accessoryType = .none
+//        }
+        
 
         return cell
         
@@ -49,15 +74,25 @@ class ToLifeDo: UITableViewController {
         
        // print(taskArray[indexPath.row])
         
+        taskArray[indexPath.row].done = !taskArray[indexPath.row].done
+//
+//        if taskArray[indexPath.row].done == false{
+//            taskArray[indexPath.row].done = true
+//        }else{
+//            taskArray[indexPath.row].done = false
+//        }
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
+        tableView.reloadData()
         
-        else {
-            
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        
+//        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+//        }
+//
+//        else {
+//
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//        }
         
         tableView.deselectRow(at: indexPath, animated: true)
         
@@ -71,8 +106,13 @@ class ToLifeDo: UITableViewController {
         let alert = UIAlertController(title: "Add", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            let newItem = TodoItem()
+            newItem.item = textField.text!
+            
+            self.taskArray.append(newItem)
     
-           self.taskArray.append(textField.text!)
+           //self.taskArray.append(textField.text!)
             
            self.defaults.setValue(self.taskArray, forKey: "TodoListArray")
             
---
+++ ToLifeDo/Data Model/TodoItem.swift
@@ -0,0 +1,17 @@
+//
+//  TodoItem.swift
+//  ToLifeDo
+//
+//  Created by Fahad Tariq on 23/08/2018.
+//  Copyright © 2018 Fahad Tariq. All rights reserved.
+//
+
+import Foundation
+
+class TodoItem {
+    
+        var item : String = ""
+        var done : Bool = false
+        
+}
+
